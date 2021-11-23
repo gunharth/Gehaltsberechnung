5edded5770ac6fbae4426c5087554b8d99b28734
A409.01 <A409.01@A409-01>
2021-11-23 20:15:44 +0100

    Formular-Design erarbeitet

--- Gehaltsberechnung/Form1.Designer.cs
+++ Gehaltsberechnung/Form1.Designer.cs
@@ -29,13 +29,68 @@ namespace Gehaltsberechnung
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtBrutto = new System.Windows.Forms.TextBox();
+            this.CmdBerechnen = new System.Windows.Forms.Button();
+            this.LblAnzeige = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Brutto";
+            // 
+            // TxtBrutto
+            // 
+            this.TxtBrutto.Location = new System.Drawing.Point(16, 29);
+            this.TxtBrutto.Name = "TxtBrutto";
+            this.TxtBrutto.Size = new System.Drawing.Size(100, 20);
+            this.TxtBrutto.TabIndex = 1;
+            // 
+            // CmdBerechnen
+            // 
+            this.CmdBerechnen.Location = new System.Drawing.Point(122, 29);
+            this.CmdBerechnen.Name = "CmdBerechnen";
+            this.CmdBerechnen.Size = new System.Drawing.Size(75, 23);
+            this.CmdBerechnen.TabIndex = 2;
+            this.CmdBerechnen.Text = "Berechnen";
+            this.CmdBerechnen.UseVisualStyleBackColor = true;
+            // 
+            // LblAnzeige
+            // 
+            this.LblAnzeige.AutoSize = true;
+            this.LblAnzeige.Location = new System.Drawing.Point(19, 65);
+            this.LblAnzeige.Name = "LblAnzeige";
+            this.LblAnzeige.Size = new System.Drawing.Size(0, 13);
+            this.LblAnzeige.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.LblAnzeige);
+            this.Controls.Add(this.CmdBerechnen);
+            this.Controls.Add(this.TxtBrutto);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtBrutto;
+        private System.Windows.Forms.Button CmdBerechnen;
+        private System.Windows.Forms.Label LblAnzeige;
     }
 }
 
